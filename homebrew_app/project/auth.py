2ccfc6f23ec5a572e98d33ae166cf935122a716c
claudette <honey20103@yahoo.com>
2020-09-24 14:36:58 +0000

    continued on signupPOST func adding imports and implementing the function/renamed file

--- homebrew_app/project/auth.py
+++ homebrew_app/project/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import os
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -12,6 +14,27 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # Try to query the db to validate if user already exists, if it returns user than user exists.
+    user = User.query.filter_by(email=email).first() 
+
+    if user: # if a user is found, we want to redirect them back to signup page so user can try again
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form model db structure. Added password hash to prevent plaintext display.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # adding the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
