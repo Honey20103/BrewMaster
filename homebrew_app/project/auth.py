fec3a02cb93a2f5edc670a49531b1bf3893c1c75
claudette <honey20103@yahoo.com>
2020-09-24 15:44:56 +0000

    added login function allow user to login and compare crednetials with db, with pass check

--- homebrew_app/project/auth.py
+++ homebrew_app/project/auth.py
@@ -15,6 +15,24 @@ def login():
 def signup():
     return render_template('signup.html')
 
+
+@auth.route('/login', methods=['POST']) 
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # first check if the user actually exists
+    # take the inputted password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please try again, credentials seem to be wrong.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    return redirect(url_for('main.dashboard'))
+
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     email = request.form.get('email')
