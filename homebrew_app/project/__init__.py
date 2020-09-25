f5f007d97c7e20069bf29fd8a17825a7a348e49f
claudette <honey20103@yahoo.com>
2020-09-25 22:18:20 +0000

    Added Usermix module to enable flask_login, and identified user session with user loader

--- homebrew_app/project/__init__.py
+++ homebrew_app/project/__init__.py
@@ -2,20 +2,34 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
+from flask_login import LoginManager
 
 # init SQLAlchemy for later use
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
 
+    app.config["DEBUG"] = True
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
     app.config["MONGO_DBNAME"] = 'brew_master'
-    app.config["MONGO_URI"] =  'mongodb+srv://root:rootbabyboy@honeycluster.v8y4e.mongodb.net/brew_master?retryWrites=true&w=majority'
+    app.config["MONGO_URI"] = 'mongodb+srv://root:rootbabyboy@honeycluster.v8y4e.mongodb.net/brew_master?retryWrites=true&w=majority'
 
     mongo = PyMongo(app)
 
--- homebrew_app/project/auth.py
+++ homebrew_app/project/auth.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 import os
-from flask import Blueprint, render_template, request, redirect, url_for, flash 
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
 from . import db
+from flask_login import login_user
+from .models import User
+
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
 
-@auth.route('/login', methods=['POST']) 
+@auth.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -28,10 +32,13 @@ def login_post():
     # take the inputted password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please try again, credentials seem to be wrong.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
     # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
     return redirect(url_for('main.dashboard'))
+
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -40,14 +47,15 @@ def signup_post():
     password = request.form.get('password')
 
     # Try to query the db to validate if user already exists, if it returns user than user exists.
-    user = User.query.filter_by(email=email).first() 
+    user = User.query.filter_by(email=email).first()
 
-    if user: # if a user is found, we want to redirect them back to signup page so user can try again
+    if user:  # if a user is found, we want to redirect them back to signup page so user can try again
         flash('It seems you are already a member')
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form model db structure. Added password hash to prevent plaintext display.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name,
+                    password=generate_password_hash(password, method='sha256'))
 
     # adding the new user to the database
     db.session.add(new_user)
@@ -55,11 +63,7 @@ def signup_post():
 
     return redirect(url_for('auth.login'))
 
+
 @auth.route('/logout')
 def logout():
     return 'Logout'
-
-if __name__ == '__main__':
-    auth.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
