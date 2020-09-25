707602308bc20e10519106ad0cdbee0b6e4756c9
claudette <honey20103@yahoo.com>
2020-09-25 22:24:38 +0000

    used the name of the current user and send it to the template with current_user function

--- homebrew_app/project/main.py
+++ homebrew_app/project/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_user
 from . import db
 
 main = Blueprint('main', __name__)
@@ -10,8 +10,9 @@ def index():
     return render_template('index.html')
 
 @main.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', name=current_user.name)
 
 if __name__ == '__main__':
     main.run(host=os.environ.get('IP'),
